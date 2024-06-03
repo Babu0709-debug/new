c70f49169db977b9f2d3b1feecfeda28242c2d33
Babu0709-debug <148149370+Babu0709-debug@users.noreply.github.com>
2024-06-03 10:22:44 +0530

    Update hellobabu.py

--- hellobabu.py
+++ hellobabu.py
@@ -37,7 +37,7 @@ class StreamlitApp:
         if st.write(self.speech_input):
             if self.df is not None:
                     agent = Agent(self.df)  # Define agent here
-                    result = agent.chat(st.write(self.speech_input))
+                    result = agent.chat(self.speech_input)
                     st.write(result)
         
 
