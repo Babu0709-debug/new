1f1312b7736505b4af6e67f0427cbffb9ea34138
Babu0709-debug <148149370+Babu0709-debug@users.noreply.github.com>
2024-06-05 11:48:04 +0530

    Update host.py

--- host.py
+++ host.py
@@ -8,7 +8,7 @@ import speech_recognition as sr
 os.environ["PANDASAI_API_KEY"] = "$2a$10$BvCK6YxJ2CH1SB1UFYCwr.JtZUXsRwUd4uiRpEL8pgU.9zGD7sRd2"
 
 uploaded_file = st.file_uploader("Upload a CSV or Excel file", type=["csv", "xlsx", "xls"])
-
+query = st.write(speech_to_text(language='en'))
 if uploaded_file is not None:
     file_extension = uploaded_file.name.split('.')[-1].lower()
     
@@ -18,7 +18,7 @@ if uploaded_file is not None:
         df = pd.read_excel(uploaded_file)
     
     st.dataframe(df.head())
-    query = st.write(speech_to_text(language='en'))
+    
     try:
         agent = Agent(df)
         
