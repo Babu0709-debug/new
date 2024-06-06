1768a40ed8b8236d93fb40513d30ad9ef2d37f95
Babu0709-debug <148149370+Babu0709-debug@users.noreply.github.com>
2024-06-07 01:36:44 +0530

    Update host.py

--- host.py
+++ host.py
@@ -8,7 +8,7 @@ import speech_recognition as sr
 os.environ["PANDASAI_API_KEY"] = "$2a$10$BvCK6YxJ2CH1SB1UFYCwr.JtZUXsRwUd4uiRpEL8pgU.9zGD7sRd2"
 
 uploaded_file = st.file_uploader("Upload a CSV or Excel file", type=["csv", "xlsx", "xls"])
-df = pd.read_csv(uploaded_file)
+df = pd.read_excel(uploaded_file)
 st.dataframe(df.head())
 query = st.write(speech_to_text(language='en'))
 agent = Agent(df)
