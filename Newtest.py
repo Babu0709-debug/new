e12c83b934fd8c33872d62b173565fb2bcf5e6a0
Babu0709-debug <148149370+Babu0709-debug@users.noreply.github.com>
2024-06-17 14:17:27 +0530

    Update Newtest.py

--- Newtest.py
+++ Newtest.py
@@ -19,13 +19,13 @@ if uploaded_file is not None:
         file_extension = uploaded_file.name.split('.')[-1].lower()
 
         if file_extension == 'csv':
-            data = pd.read_csv(uploaded_file)
+            df = pd.read_csv(uploaded_file)
         elif file_extension in ['xlsx', 'xls']:
-            data = pd.read_excel(uploaded_file)
+            df = pd.read_excel(uploaded_file)
         else:
             st.write("Unsupported file format")
         
-        df = SmartDataframe(data)
+        #df = SmartDataframe(data)
         st.dataframe(df.head())
 
         # Debug: Check the SmartDataframe initialization
